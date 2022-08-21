7d925f8cf6b1b23f91800a412ebd9459ec9ce324
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2022-08-21 14:38:27 +0200

    feat: first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+<a href="https://npmjs.com/package/@vituum/vite-plugin-juice"><img src="https://img.shields.io/npm/v/@vituum/vite-plugin-juice.svg" alt="npm package"></a>
+<a href="https://nodejs.org/en/about/releases/"><img src="https://img.shields.io/node/v/@vituum/vite-plugin-juice.svg" alt="node compatility"></a>
+
+# ⚡️🧃 ViteJuice
+
+```js
+import juice from '@vituum/vite-plugin-juice'
+
+export default {
+  plugins: [
+    juice({ 
+      paths: [],
+      tables: false,
+      options: {}
+    })
+  ]
+}
+```
+
+### Requirements
+
+- [Node.js LTS (16.x)](https://nodejs.org/en/download/)
---
+++ index.js
@@ -0,0 +1,41 @@
+import postcss from 'postcss'
+import postcssCustomProperties from 'postcss-custom-properties'
+import postcssHtml from 'postcss-html'
+import juice from 'juice'
+
+const defaultOptions = {
+    paths: [],
+    tables: false,
+    options: {}
+}
+
+const plugin = (options = {}) => {
+    options = Object.assign(options, defaultOptions)
+
+    return {
+        name: '@vituum/vite-plugin-juice',
+        enforce: 'post',
+        transformIndexHtml: {
+            enforce: 'post',
+            transform: (html, { path }) => {
+                const paths = options.paths
+
+                if (paths.length === 0 || paths.filter(p => path.startsWith(`/${p}`)).length === 0) {
+                    return html
+                }
+
+                if (options.tables) {
+                    html = html.replaceAll('<table', '<table border="0" cellpadding="0" cellspacing="0"')
+                }
+
+                const result = postcss([postcssCustomProperties({
+                    preserve: false
+                })]).process(html, { syntax: postcssHtml() })
+
+                return juice(result.content, options.options)
+            }
+        }
+    }
+}
+
+export default plugin
