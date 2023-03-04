a7bf0a01fd4843ecc91ca2fa4e1b697cbcc2e933
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2023-03-04 13:11:09 +0100

    feat: added handeLinks option

--- README.md
+++ README.md
@@ -14,7 +14,8 @@ export default {
       paths: [],
       tables: false,
       doctype: '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">',
-      juice: {}
+      options: {}, 
+      handleLinks: (href) => href
     })
   ]
 }
--- index.js
+++ index.js
@@ -2,12 +2,14 @@ import postcss from 'postcss'
 import postcssCustomProperties from 'postcss-custom-properties'
 import postcssHtml from 'postcss-html'
 import juice from 'juice'
+import parse5 from 'parse5'
 
 const defaultOptions = {
     paths: [],
     tables: false,
     doctype: '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">',
-    juice: {}
+    options: {},
+    handleLinks: (href) => href
 }
 
 const plugin = (userOptions = {}) => {
@@ -18,11 +20,31 @@ const plugin = (userOptions = {}) => {
         enforce: 'post',
         transformIndexHtml: {
             enforce: 'post',
-            transform: (html, { path }) => {
+            transform: async (html, { path }) => {
                 const paths = userOptions.paths
+                let extraCss = ''
 
                 if (paths.length === 0 || paths.filter(p => path.startsWith(`/${p}`)).length === 0) {
                     return html
+                }
+
+                if (html.includes('data-juice-link')) {
+                    const document = parse5.parse(html)
+                    const headNodes = document.childNodes[1].childNodes[0].childNodes
+                    const headLinks = headNodes.filter(({ nodeName, attrs }) => nodeName === 'link' && attrs.filter(({name}) => name === 'data-juice-link'))
+
+                    for (const link of headLinks) {
+                        const href = link.attrs.filter(({ name }) => name === 'href')[0].value
+
+                        if (typeof userOptions.handleLinks === 'function') {
+                            extraCss += await userOptions.handleLinks(href)
+                        }
+
+                        headNodes.splice(headNodes.indexOf(link), 1);
+                    }
+
+                    html = parse5.serialize(document)
+                    html = html.replace('</head>', `<style>${extraCss}</style></head>`)
                 }
 
                 html = html.replace('<!DOCTYPE html>', userOptions.doctype)
