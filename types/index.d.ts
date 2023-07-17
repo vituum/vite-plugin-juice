a5a2ae879d87eb1aa442cc6220fd3b7eba511e17
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2023-07-17 10:59:24 +0200

    Squashed commit of the following:
    commit 1b7e94b8a050ed9fea2e786387442d1319024f88
    Merge: 0c1ed5b 6afcd3a
    Author: Lubomír Blažek <lubomir.blazek@newlogic.cz>
    Date:   Mon Jul 17 10:58:57 2023 +0200
    
        Merge branch 'main' into next
    
    commit 0c1ed5b562adb23f5e933da5dd9210f55cc85d83
    Author: Lubomír Blažek <lubomir.blazek@newlogic.cz>
    Date:   Mon Jul 17 10:53:54 2023 +0200
    
        feat: added postcssGlobalData
    
    commit e19cd0c1a534205c2138fbb9031532d3f61c888d
    Author: Lubomír Blažek <lubomir.blazek@newlogic.cz>
    Date:   Tue Jun 27 10:29:15 2023 +0200
    
        feat: 1.0
    
    commit 5b95c48a8a0765b8d8c7a68bd67df2b73915778a
    Author: Lubomír Blažek <lubomir.blazek@newlogic.cz>
    Date:   Fri Jun 23 13:29:37 2023 +0200
    
        feat: beta.1
    
    commit 4b24848349f34f4efee2ddc5392b20f203a37bc6
    Author: Lubomír Blažek <lubomir.blazek@newlogic.cz>
    Date:   Thu Jun 22 16:45:12 2023 +0200
    
        feat: edits
    
    commit ee9709acc3b527be3fb735b879a58601df31b760
    Author: Lubomír Blažek <lubomir.blazek@newlogic.cz>
    Date:   Mon Jun 19 15:35:32 2023 +0200
    
        feat: updates
    
    commit c8835cd83ae7e2ae7273009f65f247e45eb80810
    Author: Lubomír Blažek <lubomir.blazek@newlogic.cz>
    Date:   Thu Jun 15 16:26:59 2023 +0200
    
        feat: 1.0 rewrite

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,6 +2,10 @@ export interface PluginUserConfig {
     paths?: string[]
     tables?: boolean
     doctype?: string
+    postcss?: {
+        globalData?: import('@csstools/postcss-global-data').pluginOptions
+        customProperties?: import('postcss-custom-properties').pluginOptions
+    }
     options?: import('juice').Options
     juiceLink?: (href: string) => string | Promise<string>
 }
